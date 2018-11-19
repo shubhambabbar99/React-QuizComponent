f27bcba3489a9d0eda082067eae3e6b71d31a93f
shubhambabbar99 <shubhambabbar62@gmail.com>
2018-11-19 14:40:30 +0530

    add

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,44 +1,46 @@
 import React, { Component } from 'react';
 
-let quizData = require ('./quiz_data.json');
+let quizData = require('./quiz_data.json');
 
 
 
 
 class Quiz extends Component {
 
-    constructor (props){
+    constructor(props) {
 
-super (props);
-this.setState ({quiz_position: 1})
+        super(props);
+        this.state={ 
+            quiz_position: 1 
+        }
 
     }
 
-render () {
+    render() {
 
 
-return (
+        return (
 
-<div>
-    
-    
-    <div className="QuizQuestion">
-
-{quizData.quiz_questions[0].instruction_text}
-
-    </div>
-    
-    
-    
-    </div>
-
-)
+            <div>
 
 
-   
+                <div className="QuizQuestion">
+
+                    {quizData.quiz_questions[0].instruction_text}
+
+                </div>
 
 
-}
+
+            </div>
+
+        )
+
+
+
+
+
+    }
 
 }
 
